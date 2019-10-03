938095f800acefd4032599da5a901c5dd07de358
unknown <mattb1212000@gmail.com>
2019-10-03 16:49:08 -0500

    first commit

--- Documents/Arduino/sketch_sep26a/sketch_sep26a.ino
+++ Documents/Arduino/sketch_sep26a/sketch_sep26a.ino
@@ -1,13 +1,13 @@
 #include <KNWRobot.h>
 
 int tempPin = 0; //voltage divider tap connected to analog pin 0
-KNWRobot* robot;
+KNWRobot* myRobot;
 bool success;
 
 void setup() {
   // put your setup code here, to run once:
-  robot = new KNWRobot();//create instance of the robot
-  success = robot->setupTemp(tempPin);//setup probe with input pin
+  myRobot = new KNWRobot();//create instance of the robot
+  success = myRobot->setupTemp(tempPin);//setup probe with input pin
   if (success){
     Serial.begin(9600);//setup serial communication with laptop
     }
@@ -20,7 +20,7 @@ void loop() {
   if (success) {
     adc = 0;
     for (i = 0;i < 10;i++){//read adc 10 times
-      adc += robot->getTemp();//read adc for temperature probe
+      adc += myRobot->getTemp();//read adc for temperature probe
       }
       Serial.println(adc / 10); //report average value to the serial port
     }
