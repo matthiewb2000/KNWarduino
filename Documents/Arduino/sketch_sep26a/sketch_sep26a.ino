6eca8f61393186de216fc7940946bfcb057d9a38
unknown <mattb1212000@gmail.com>
2019-10-08 17:20:39 -0500

    new commit

--- Documents/Arduino/sketch_sep26a/sketch_sep26a.ino
+++ Documents/Arduino/sketch_sep26a/sketch_sep26a.ino
@@ -1,6 +1,8 @@
 #include <KNWRobot.h>
 
 int tempPin = 0; //voltage divider tap connected to analog pin 0
+int motorPinIn = 2;//motor pin assigned to 2(note subject to change
+int motorPinOut = 3;//motor pin assigned to 3(note subject to change)
 KNWRobot* myRobot;
 bool success;
 
@@ -11,6 +13,13 @@ void setup() {
   if (success){
     Serial.begin(9600);//setup serial communication with laptop
     }
+
+
+  pinMode(motrPinIn,INPUT);
+  pinMode(motorPinOut,OUTPUT);
+  Serial.begin(9600);
+   While (!Serial);
+   Serial.println("speed 0 to 255");
 }
 
 void loop() {
@@ -25,4 +34,14 @@ void loop() {
       Serial.println(adc / 10); //report average value to the serial port
     }
     delay(1000); //wait a second
+
+    
+    digitalWrite(motorPin,HIGH);
+    if (Serial.available()){
+      int speed = Serial.parseInt();
+      if (speed >= 0 && speed <= 255){
+        analogWrite(motorPin,speed);
+        }
+      
+      }
 }
